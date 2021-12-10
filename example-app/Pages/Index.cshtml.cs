89113a8e22eed5523df8a803fdb12fe5764aa9d0
Rui Lopes <rgl@ruilopes.com>
2021-12-10 13:53:32 +0000

    show the server and client geo ip information

--- example-app/Pages/Index.cshtml.cs
+++ example-app/Pages/Index.cshtml.cs
@@ -2,12 +2,22 @@
 
 namespace ExampleApp.Pages;
 
+public class GeoIp
+{
+    public string? Ip { get; set; }
+    public string? Country { get; set; }
+    public string? Region { get; set; }
+    public string? City { get; set; }
+    public string? Organization { get; set; }
+}
+
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
     private readonly IHttpClientFactory _httpClientFactory;
 
-    public string ServerPublicIpAddress { get; private set; } = "";
+    public GeoIp? ServerPublicGeoIp { get; private set; }
+    public GeoIp? ClientPublicGeoIp { get; private set; }
 
     public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
     {
@@ -19,6 +29,7 @@ public class IndexModel : PageModel
     {
         var client = _httpClientFactory.CreateClient();
 
-        ServerPublicIpAddress = await client.GetStringAsync("https://ip.seeip.org/");
+        ServerPublicGeoIp = await client.GetFromJsonAsync<GeoIp>("https://ip.seeip.org/geoip");
+        ClientPublicGeoIp = await client.GetFromJsonAsync<GeoIp>($"https://ip.seeip.org/geoip/{Uri.EscapeDataString(HttpContext.Connection.RemoteIpAddress?.ToString() ?? "")}");
     }
 }
